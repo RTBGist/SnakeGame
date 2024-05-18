c6707fcc320f86bed0015373b05ddf9b2fa98da8
DESKTOP-8IENF4O\User <yourdeveloper@mail.ru>
2024-05-19 01:51:07 +0700

    add moving snake by a,w,s,d

--- src/components/Snake/Snake.tsx
+++ src/components/Snake/Snake.tsx
@@ -3,48 +3,39 @@ import {Cell} from "../Cell/Cell";
 import {Coord, DirectionType} from "../../types/types";
 
 import classes from './Snake.module.scss';
-import {calcCoord} from "../../libs/calcCoord";
+import {calcStepPX} from "../../libs/calcStepPX";
 
 export const Snake = () => {
 	const [direction, setDirection] = useState<DirectionType>('right');
-	const [coord, setCoord] = useState<Coord[]>([{x: 0, y: 0}]);
+	const [coord, setCoord] = useState<Coord[]>([{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}]);
 
 	useEffect(() => {
 		const onKeyPress = (event) => {
 			switch (event.key.toLowerCase()) {
 				case 'a':
-					setCoord((coord) => {
-						return coord.map(({x, y}) => {
-							return {x: x - 1, y}
-						})
-					})
+					setCoord(coord.map((point, index) =>
+							index === coord.length - 1 ? { x: point.x - 1, y: point.y } : coord[index + 1]
+					));
 					break;
 				case 'd':
-					setCoord((coord) => {
-						return coord.map(({x, y}) => {
-							return {x: x + 1, y}
-						})
-					})
+					setCoord(coord.map((point, index) =>
+							index === coord.length - 1 ? { x: point.x + 1, y: point.y } : coord[index + 1]
+					));
 					break;
 				case 'w':
-					setCoord((coord) => {
-						return coord.map(({x, y}) => {
-							return {x: x, y: y - 1}
-						})
-					})
+					setCoord(coord.map((point, index) =>
+							index === coord.length - 1 ? { x: point.x, y: point.y - 1 } : coord[index + 1]
+					));
 					break;
 				case 's':
-					setCoord((coord) => {
-						return coord.map(({x, y}) => {
-							return {x: x, y: y + 1}
-						})
-					})
+					setCoord(coord.map((point, index) =>
+							index === coord.length - 1 ? { x: point.x, y: point.y + 1 } : coord[index + 1]
+					));
 					break;
 			}
 		}
 
 		document.addEventListener('keypress', onKeyPress)
-
 		return () => {
 			document.removeEventListener('keypress', onKeyPress)
 		}
@@ -53,7 +44,7 @@ export const Snake = () => {
 	return (
 			<>
 				{coord.map((cell, i) => {
-					const {x, y} = calcCoord(coord[i]);
+					const {x, y} = calcStepPX(coord[i]);
 
 					return <Cell className={classes.cell} style={{backgroundColor: 'green', left: x, top: y}} key={i} />
 				})}
--- src/libs/calcStepPX.ts
+++ src/libs/calcStepPX.ts
@@ -1,6 +1,6 @@
 import {Coord} from "../types/types";
 
-export function calcCoord(coord: Coord) {
+export function calcStepPX(coord: Coord) {
 	return {
 		x: coord.x ? coord.x * 50 + 'px' : '0px',
 		y: coord.y ? coord.y * 50 + 'px' : '0px'
